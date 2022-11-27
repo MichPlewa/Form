e00c538d227f6fdf38940d5a91010c56e95ae7a3
Michal Plewa <themipsial@gmail.com>
2022-11-27 01:44:40 +0100

    rebuild

--- App.tsx
+++ App.tsx
@@ -9,37 +9,28 @@ import {
 } from 'react-native';
 import { useState } from 'react';
 import { launchImageLibrary } from 'react-native-image-picker';
-import LinearGradient from 'react-native-linear-gradient';
+import { LinearGradient } from 'expo-linear-gradient';
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
 
 export default function App() {
   const [name, setName] = useState('');
   const [surname, setSurname] = useState('');
   const [pesel, setPesel] = useState('');
   const [nip, setNip] = useState('');
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
-  console.log(name);
-  const options = {
-    title: 'Select Image',
-    type: 'library',
-    options: {
-      maxHeight: 100,
-      maxWidth: 100,
-      selectionLimit: 0,
-      mediaType: 'photo',
-      includeBase64: false,
-      includeExtra: false,
-    },
-  };
+  const [isOsoba, setIsOsoba] = useState(false);
   const openGallery = async () => {
-    const result = await launchImageLibrary(options);
+    const result = await launchImageLibrary({
+      selectionLimit: 1,
+      mediaType: 'photo',
+      includeBase64: true,
+    });
     console.log(result);
   };
   const handleSubmit = () => {
     const fd = new FormData();
     fd.append('name', name);
     fd.append('surrname', surname);
-    if (!isEnabled) fd.append('pesel', pesel);
+    if (!isOsoba) fd.append('pesel', pesel);
     else fd.append('nip', nip);
 
     const fetchOptions = {
@@ -54,89 +45,101 @@ export default function App() {
       });
   };
 
+  const indexOfselected = () => {
+    if (isOsoba) return 1;
+    else return 0;
+  };
+
   return (
     <View style={styles.container}>
-      {/* <LinearGradient
-        colors={['#c848db', '#9448db']}
+      <LinearGradient
         style={styles.linearGradient}
+        colors={['#5600f5', '#9b00f5']}
         start={{ x: 0, y: 0.5 }}
         end={{ x: 1, y: 0.5 }}
-      ></LinearGradient> */}
-      <Text style={styles.title}>Dodaj Kontrahenta:</Text>
-      <TextInput
-        placeholder="Imię"
-        style={styles.form}
-        onChangeText={(e) => {
-          setName(e);
-        }}
-      />
-      <TextInput
-        placeholder="Nazwisko"
-        style={styles.form}
-        onChangeText={(e) => {
-          setSurname(e);
-        }}
-      />
-      {!isEnabled ? (
-        <TextInput
-          placeholder="Pesel"
-          style={styles.form}
-          onChangeText={(e) => {
-            setPesel(e);
-          }}
-        />
-      ) : (
-        <TextInput
-          placeholder="NIP"
-          style={styles.form}
-          onChangeText={(e) => {
-            setNip(e);
-          }}
-        />
-      )}
-      <View style={styles.switch}>
-        <Text>Osoba</Text>
-        <Switch
-          trackColor={{ false: '#767577', true: '#81b0ff' }}
-          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-        />
-        <Text>Firma</Text>
-      </View>
-      <View style={styles.container1}>
-        <View style={styles.imgBtn}>
-          <Button title="Dodaj Zdjęcie" onPress={() => openGallery()} />
+      >
+        <View style={styles.bc}>
+          <Text style={styles.title}>Dodaj Kontrahenta</Text>
+          <SegmentedControl
+            values={['Osoba', 'Firma']}
+            selectedIndex={indexOfselected()}
+            onChange={(e) => {
+              if (e.nativeEvent.value === 'Osoba') setIsOsoba(false);
+              else setIsOsoba(true);
+            }}
+            style={{ marginBottom: 16 }}
+          />
+          <TextInput
+            placeholder="Imię"
+            style={styles.form}
+            onChangeText={(e) => {
+              setName(e);
+            }}
+          />
+          <TextInput
+            placeholder="Nazwisko"
+            style={styles.form}
+            onChangeText={(e) => {
+              setSurname(e);
+            }}
+          />
+          {!isOsoba ? (
+            <TextInput
+              placeholder="Pesel"
+              style={styles.form}
+              onChangeText={(e) => {
+                setPesel(e);
+              }}
+            />
+          ) : (
+            <TextInput
+              placeholder="NIP"
+              style={styles.form}
+              onChangeText={(e) => {
+                setNip(e);
+              }}
+            />
+          )}
+          <View></View>
+          <View style={styles.container1}>
+            <View style={styles.imgBtn}>
+              <Button title="Dodaj Zdjęcie" onPress={() => openGallery()} />
+            </View>
+            <View>
+              <Image
+                style={styles.img}
+                source={require('./assets/favicon.png')}
+              />
+            </View>
+          </View>
+          <Button
+            title="Wyślij"
+            color="#9ef500"
+            onPress={() => handleSubmit()}
+          />
         </View>
-        <View>
-          <Image style={styles.img} source={require('./assets/favicon.png')} />
-        </View>
-      </View>
-      <View style={styles.sendBtn}>
-        <Button title="Wyślij" onPress={() => handleSubmit()} />
-      </View>
+      </LinearGradient>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   linearGradient: {
+    flex: 1,
+    width: '100%',
+    padding: 16,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 5,
-    height: '100%',
-    width: '350%',
+  },
+  bc: {
+    backgroundColor: 'rgba(215, 179, 252, 1)',
+    padding: 64,
+    borderRadius: 10,
   },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 64,
-    backgroundColor: '#7206d6',
   },
   container1: {
-    width: '70%',
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingTop: 10,
@@ -151,11 +154,10 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
   },
   form: {
-    width: '70%',
     padding: 10,
     marginBottom: 25,
-    borderWidth: 2,
-    borderColor: 'lightgray',
+    borderRadius: 10,
+    backgroundColor: '#ffff',
   },
   imgBtn: {
     justifyContent: 'flex-start',
@@ -164,5 +166,4 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
   },
-  sendBtn: {},
 });
